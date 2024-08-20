4f8763b2e601be87e8fcb483639cec1aac3919bb
Stephen Weybrecht <stephenweybrecht@gmail.com>
2024-08-20 17:22:04 +0200

    updates to Tex and significant changes to code

--- routines.py
+++ routines.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from tqdm import tqdm
-from corrections import pattern_correction, lowpass
+from corrections import *
 from scipy.optimize import curve_fit
 from scipy.signal import correlate
 from scipy.interpolate import interp1d
@@ -71,7 +71,7 @@ class Data_reduction:
         freq = np.fft.rfftfreq(len(self.av_g2), self._sampling_time)
         return freq, fft_amplitude
     
-    def integrate(self, return_infodoct:bool=False, plot_fits:bool=False):
+    def integrate(self, return_infodict:bool=False, plot_fits:bool=False, external_plotting:bool=False):
         '''
         Method that calculates the integral of the bunching peak in the average g2 data and its error. The error on the integral is calculated by shifting the bunching peak to different positions, fitting and integrating the fit for each shift. 
         The standard deviation of these integrals is the error on the integral.
@@ -80,76 +80,96 @@ class Data_reduction:
         Takes: 
             - return_infodict: bool, optional; if True additional information will be returned. Default is False. 
             - plot_fits: bool, optional; if True some fits will be plotted for debugging purposes. Default is False.
+            - external_plotting: bool, optional; if True the shifted g2 data, fits and integrals and corresponding time shifts will be set as class variables for external plotting. Default is False.
             
         Returns:     
             - If return_infodict, a dictionary containing the following will be returned:
                 - integral: tuple, the integral of the bunching peak and its error.
-                - fit_params: list, contains integral fit parameters and errors. fit_params[i] = (popt[i], pcov[i,i]**0.5) where i is amplitude, x_shift, sigma.
+                - fit_params: list, contains integral fit parameters and errors. fit_params[i] = [popt[i], pcov[i,i]**0.5] where i is amplitude, x_shift, sigma.
                 - lf_pattern: np.array, the lowpass filter pattern that was subtracted from the average g2 data.
-                - shifts: np.array, the shifts relative to the peak position that were used for the error calculation.
-                - err_integrals: np.array, the integrals of the bunching peak for each shift.
         '''
         # ----------- (Changing these values will need changes in for loop!) -------------
         fitting_width = 100 # fit data in +/- 100 bins (160ns) around peak
-        integration_width = 25  # integrate data +/- 25 bins (40ns) around peak
+        integration_width = 100  # integrate data +/- 25 bins (40ns) around peak
         # --------------------------------------------------------------------------------
         self._set_correlated_pulse_function() # Sets self.corr_func, needed for fitting
         g2, t = self.av_g2, self.time_arr
-        lf_pattern = lowpass(g2, 0.75e-3)
+        lf_pattern = lowpass(g2, 0.60e-3) # 60kHz for lf pattern
         g2 = g2 - lf_pattern
+        self.av_g2_corr = g2
         peak_pos = np.argmax(g2)
         # Cut peak from data
         t_fit = t[peak_pos-fitting_width:peak_pos+fitting_width]
         g2_fit = g2[peak_pos-fitting_width:peak_pos+fitting_width]
         
         # ----------------- Fit and integration of bunching peak -------------------------
-        p0 = [3e-6, t[peak_pos], 1e-9]
+        p0 = [6e-6, t[peak_pos], 1e-9]
         popt, pcov  = curve_fit(self._fit_func, t_fit, g2_fit, p0=p0)
-        fit = self._fit_func(t[peak_pos-25: peak_pos+25], *popt)
-        integral = np.trapz(fit, dx=self._sampling_time)
+        fit = self._fit_func(t[peak_pos-integration_width : peak_pos+integration_width], *popt)
+        integral = np.trapz(self._fit_func(np.arange(t[peak_pos-integration_width], t[peak_pos+integration_width],0.1e-9), *popt), dx=0.1e-9)
 
         if plot_fits:
             import matplotlib.pyplot as plt
             cmap = plt.get_cmap('viridis')
-            fig,ax = plt.subplots(figsize=(10,5))  
+            fig,ax = plt.subplots()  
             ax.plot(t*1e9, g2)
             ax.plot(t_fit*1e9, self._fit_func(t_fit, *popt), color='r')
             ax.set(xlabel='Time [ns]', ylabel='g2')
+            
         # -------------------- Get error on integral -------------------------------------
-        # Array of all shifts relative to peak_pos in bins. Centers around peak_pos so that shift_range ~ [-4000:peak_pos_bin-2*fitting_width, peak_pos_bin+2*fitting_width:4000]. This way the region around peak_pos is always excluded.
-        # peak_pos_bin is the index of the peak in g2 and not its time bin. It's equal to peak_pos-5000 as len(g2)=10000
-        shift_range = np.append(np.flip(np.arange(peak_pos-5000-2*fitting_width, -4000, -50)), np.arange(peak_pos-5000+2*fitting_width, 4000,50)).astype(int)
+        # Array of all shifts relative to peak_pos in bins. Centers around peak_pos so that shift_range ~ [-3750:peak_pos_bin-2*fitting_width, peak_pos_bin+2*fitting_width:3750]. This way the region around peak_pos is always excluded.
+        # peak_pos is the index of the peak in g2 and not its time bin. To get a time bin we must therefore use peak_pos-5000 as len(g2)=10000.
+        # Np.flip is used to make the array symmetric around peak_pos.
+        shift_range = np.append(np.flip(np.arange(peak_pos-5000-2*fitting_width, -3750, -25)), np.arange(peak_pos-5000+2*fitting_width, 3750,25)).astype(int)
         # Start integration for all shifts in shift_range
-        I = np.zeros_like(shift_range, dtype=float) # Array where I[n] is the integral for n=shift_range[n]
+        I = np.zeros_like(shift_range, dtype=float)    # Array where I[n] is the integral for n=shift_range[n]
+        # Other stuff for external plottin gwhich will end up in infodict
+        shifted_g2 = np.zeros((len(shift_range),200), dtype=float)   
+        fits = np.zeros((len(shift_range),200), dtype=float)  
+        t_shifted = np.zeros((len(shift_range),200), dtype=float)
         for n, shift in enumerate(shift_range):
             # Get start and stop indices for fit for each shift
             start_index = len(t)//2 + shift
             stop_index = start_index + 2*integration_width
             # Make array of size 2*fitting_width, place fit at centre, add to g2 -> shift_g2: 
             fit_arr = np.zeros(200)
-            fit_arr[75:125] = fit
-            shift_g2 = np.copy(g2[start_index-75:stop_index+75]) # Needed, else g2 is modified
+            fit_arr[:] = fit
+            shift_g2 = np.copy(g2[start_index-0:stop_index+0]) # Needed, else g2 is modified
             shift_g2 += fit_arr
             
             # Fitting + Integration: 
-            peak_pos = t[start_index-75:stop_index+75][np.argmax(shift_g2)] # peak position of shifted fit in s, used for fit
-            popt_err, _ = curve_fit(self._fit_func, t[start_index-75:stop_index+75], shift_g2, p0=[2e-6, peak_pos, 2e-9])
-            I[n] = np.trapz(self._fit_func(t[start_index:stop_index], *popt_err), dx=self._sampling_time)
+            peak_pos = t[start_index-0:stop_index+0][np.argmax(shift_g2)] # peak position of shifted fit in s, used for fit
+            popt_err, _ = curve_fit(self._fit_func, t[start_index-0:stop_index+0], shift_g2, p0=[6e-6, peak_pos, 1e-9])
+            I[n] = np.trapz(self._fit_func(np.arange(t[start_index],t[stop_index],0.1e-9), *popt_err), dx=0.1e-9)
+            if external_plotting:
+                shifted_g2[n,:] = shift_g2
+                fits[n,:] = self._fit_func(t[start_index:stop_index], *popt_err)
+                t_shifted[n,:] = t[start_index:stop_index]
             if plot_fits and n%5==0:
-                ax.plot(t[start_index-75:stop_index+75]*1e9, shift_g2, color=cmap(n/len(shift_range)))
+                ax.plot(t[start_index-0:stop_index+0]*1e9, shift_g2, color=cmap(n/len(shift_range)))
                 ax.plot(t[start_index:stop_index]*1e9, self._fit_func(t[start_index:stop_index], *popt_err), color='k', linestyle='--')
-        self.integral = integral, np.std(I)
-        if return_infodoct:
-            return {'integral': (integral, np.std(I)), 'fit_params': [(popt[i], pcov[i,i]**0.5) for i in range(3)], 'lf_pattern': lf_pattern, 'shifts': shift_range, 'err_integrals': I}
+        self.integral = np.array([integral, np.std(I)])
+        
+        if external_plotting:
+            self.shift_range = shift_range 
+            self.err_integrals = I
+            self.shifted_g2 = shifted_g2
+            self.shifted_fits = fits
+            self.t_shifted = t_shifted
+        if return_infodict:
+            return {'integral': (integral, np.std(I)), 'fit_params': popt, 'fit_params_error': np.diag(pcov)**0.5, 'lf_pattern': lf_pattern}
 
             
     def _fit_func(self, x, amplitude, x_shift, sigma):
         '''
         Helper method that contains the fitting function for the bunching peak.
         '''
-        f = amplitude*self._corr_func(x - x_shift) # Interpolated, correlated mean pulse shape
-        g = np.exp(-(np.arange(-25e-9,25e-9,self._sampling_time))**2/(2*sigma**2)) # Gaussian smoothing kernel
-        return np.convolve(f,g,mode='same')
+        samples = np.arange(-50,50,1)*self._sampling_time     # Samples for interpolation
+        f = self._corr_func(samples)                          # Interpolated, correlated mean pulse shape
+        g = np.exp(-(samples)**2/(2*sigma**2))                # Gaussian smoothing kernel
+        conv = np.convolve(f,g,mode='same')                   # Convolve, then interpolate for function thats defined for all x
+        interp = interp1d(samples, conv, fill_value=0, kind='linear', bounds_error=False)  
+        return amplitude * interp(x-x_shift)/np.max(interp(x-x_shift)) # Normalisation makes amplitude par. of fit more meaningful
     
     def _set_correlated_pulse_function(self) -> None:
         '''
@@ -159,7 +179,7 @@ class Data_reduction:
         pulse = correlate(shape1[:,1], shape0[:,1], mode='same')
         # Normalisation and shifting the peak to t=0 for easier fitting:
         pulse= pulse/np.max(pulse) 
-        x = np.arange(0,211)*1e-9
+        x = np.arange(0,211)*self._sampling_time
         x = x - x[np.argmax(pulse)]
         # Interpolation:
         self._corr_func = interp1d(x, pulse, fill_value=0.0, kind='linear', bounds_error=False)
@@ -205,4 +225,3 @@ class Data_reduction:
         ch0 = np.loadtxt(os.path.join(self._raw_dir, 'ch0', 'calib.shape1'))
         ch1 = np.loadtxt(os.path.join(self._raw_dir, 'ch1', 'calib.shape1'))
         return ch0, ch1
-        
